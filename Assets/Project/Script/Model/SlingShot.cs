368b604e69d4549b68197f0ebc5a5fc0b0f3ff2d
辉明 袁 <690822046@qq.com>
2021-10-22 11:31:11 +0800

    UI素材上传

---
+++ Assets/Project/Script/Model/SlingShot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Farme;
+namespace Angry_Birds
+{
+    public class SlingShot : BaseMono
+    {      
+        protected override void Awake()
+        {
+            base.Awake();
+            RegisterComponentsTypes<LineRenderer>();
+        }
+
+        protected override void Start()
+        {
+            base.Start();
+
+
+        }
+
+
+        public void RendererLine( )
+        {
+
+        }
+    }
+}
