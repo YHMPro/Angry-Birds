00e73b0e1fa5ae0dd14aed2047bf8e87d45eada1
YHMPro <690822046@qq.com>
2022-05-27 21:25:51 +0800

    重新构建AB包

--- Assets/Project/Script/LoginEntry.cs
+++ Assets/Project/Script/LoginEntry.cs
@@ -7,19 +7,29 @@ using Bird_VS_Boar.Data;
 using Farme.UI;
 using UnityEngine.UI;
 using Farme.Tool;
+using Farme.Net;
 namespace Bird_VS_Boar
 {
     public class LoginEntry : MonoBehaviour
     {
         private void Awake()
-        {          
+        {
             //Debuger.Enable = false;
-            AssetBundleLoad.PackageCatalogueFile_URL = Application.streamingAssetsPath+"/";
+
+            AssetBundleLoad.PackageCatalogueFile_URL = Application.streamingAssetsPath + "/";
             AssetBundleLoad.MainABName = "StandaloneWindows";
             LevelConfigManager.ReadConfigTableData();//读取配置表数据
             MonoSingletonFactory<ShareMono>.GetSingleton(null, false);
             //MonoSingletonFactory<DataManager>.GetSingleton(null, false);
             NotMonoSingletonFactory<OtherConfigInfo>.GetSingleton().InitConfigInfo();//创建单例并实例化配置信息
+            //WebDownloadTool.WebDownloadText(@"http://localhost/HotFix.txt", (hotfix) =>
+            //{
+            //    //LuaEnv luaEnv=new LuaEnv();
+            //    //luaEnv.DoString(hotfix);
+            //    Debuger.Log(hotfix);
+             
+            //});
+          
         }
 
         private void Start()
